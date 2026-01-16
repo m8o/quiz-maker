11a04018ba8c326e94bcdd748dabcc95f3075991
m8o <mate@matezelenika.com>
2026-01-16 10:52:39 +0100

    Create a mock client with mocked data and logic

---
+++ src/api/mockClient.js
@@ -0,0 +1,47 @@
+const initialQuestions = [
+  {
+    id: 1,
+    question: "Who was the English mathematician...?",
+    answer: "Ada Lovelace",
+  },
+  { id: 2, question: "What is the capital of France?", answer: "Paris" },
+];
+const initialQuizzes = [
+  {
+    id: 1,
+    name: "General Knowledge",
+    questions: [initialQuestions[0], initialQuestions[1]],
+  },
+];
+
+let questions = [...initialQuestions];
+let quizzes = [...initialQuizzes];
+
+export const mockClient = {
+  getQuizzes: async () => quizzes,
+  createQuiz: async (quiz) => {
+    quizzes.push(quiz);
+    const newQuestions = quiz.questions.filter(
+      (q) => !questions.find((existingQ) => existingQ.id === q.id)
+    );
+    questions = questions.concat(newQuestions);
+    return "success";
+  },
+  getSingleQuiz: async (id) => {
+    quizzes.find((quiz) => quiz.id === id);
+    return "success";
+  },
+  updateSingleQuiz: async (quiz) => {
+    quizzes = quizzes.map((q) => (q.id === quiz.id ? quiz : q));
+    const newQuestions = quiz.questions.filter(
+      (q) => !questions.find((existingQ) => existingQ.id === q.id)
+    );
+    questions = questions.concat(newQuestions);
+    return "success";
+  },
+  deleteQuiz: async (id) => {
+    quizzes = quizzes.filter((quiz) => quiz.id !== id);
+    return "success";
+  },
+  getQuestions: async () => questions,
+};
