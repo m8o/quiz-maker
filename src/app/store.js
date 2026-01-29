0ec6fc698628e3acae58011d224c596ce01bf3d4
m8o <mate@matezelenika.com>
2026-01-29 14:50:34 +0100

    Create ui redux slice and introduce it to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import quizzesReducer from "../features/quizzes/quizzesSlice";
+import uiReducer from "../features/ui/uiSlice";
 
 export const store = configureStore({
   reducer: {
     quizzes: quizzesReducer,
+    ui: uiReducer,
   },
 });
